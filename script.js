1ad3cca4093ccfee130e070669596146a5bda093
AlbertCraciun <73995361+AlbertCraciun@users.noreply.github.com>
2024-12-02 15:36:06 +0200

    new_version incomming

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 // script.js
 
-const dropdownBtn = document.querySelector(".dropdown-btn");
-const dropdownContent = document.querySelector(".dropdown-content");
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdownBtn = document.querySelector(".dropdown-btn");
+    const dropdownContent = document.querySelector(".dropdown-content");
+    const scrollToTopButton = document.querySelector("#to-top button");
 
-dropdownBtn.addEventListener("click", () => {
-    dropdownContent.style.display =
-        dropdownContent.style.display === "block" ? "none" : "block";
-    dropdownBtn.classList.toggle("active");
+    dropdownBtn.addEventListener("click", () => {
+        dropdownContent.style.display =
+            dropdownContent.style.display === "block" ? "none" : "block";
+        dropdownBtn.classList.toggle("active");
+    });
+
+    scrollToTopButton.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
 });
